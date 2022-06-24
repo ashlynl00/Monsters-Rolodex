24740746f7de34204554617638bd99484b39c50d
Ashlyn Lanaville <ashlyn@downing.us>
2022-06-24 14:23:46 -0700

    added background and search css

--- src/components/search-box/search-box.jsx
+++ src/components/search-box/search-box.jsx
@@ -1,11 +1,12 @@
 import { Component } from "react";
+import './search-box.styles.css';
 
 class SearchBox extends Component {
 
     render() {
         return (
             <input 
-                //className='search-box' 
+                className='monsters-search-box search-box' 
                 type='search' 
                 placeholder='search monsters' 
                 onChange={this.props.onSearchChange}>
